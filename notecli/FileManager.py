7b8af7f646831882a8b76053bed7c55c01b59095
Liorinio <im.lior.koren@gmail.com>
2026-08-09 17:18:46 +0300

    removing file_path from functions' signatures

--- notecli/FileManager.py
+++ notecli/FileManager.py
@@ -6,21 +6,23 @@ from notecli.note_dict import NoteDict
 logger = logging.getLogger(__name__)
 
 class DbFileStorage:
+    file_path = "db2.json"
+
     @staticmethod
-    def save_to_db(nt: NoteDict, file_path: str):
+    def save_to_db(nt: NoteDict):
         data = {"db_data": nt.get_db_data(),"counter": nt.get_counter()}
 
-        Path(file_path).write_text(json.dumps(data, indent=4, default=lambda obj : obj.name))
-        logger.info(f"The database was saved to a file in the following path: {file_path}")
+        Path(DbFileStorage.file_path).write_text(json.dumps(data, indent=4, default=lambda obj : obj.name))
+        logger.info(f"The database was saved to a file in the following path: {DbFileStorage.file_path}")
 
     @staticmethod
-    def load_from_json(file_path: str) -> NoteDict:
-        path = Path(file_path)
+    def load_from_json() -> NoteDict:
+        path = Path(DbFileStorage.file_path)
 
         if not path.exists() or path.stat().st_size == 0:
-            logger.info(f"The database wasn't existed in following path: {file_path}, hence it was created")
+            logger.info(f"The database wasn't existed in following path: {DbFileStorage.file_path}, hence it was created")
             return NoteDict(db_data=[], counter=0)
 
         data = json.loads(path.read_text(encoding="utf-8"))
-        logger.info(f"The database was read from the following path: {file_path}")
+        logger.info(f"The database was read from the following path: {DbFileStorage.file_path}")
         return NoteDict(db_data=data["db_data"],counter=data["counter"])
