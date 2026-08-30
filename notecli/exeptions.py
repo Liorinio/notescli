390dcc49b9c7e2a085cad467bddac804b1cee6d7
Liorinio <im.lior.koren@gmail.com>
2026-08-30 17:32:55 +0300

    editing openapi.yaml

--- notecli/exeptions.py
+++ notecli/exeptions.py
@@ -2,6 +2,7 @@ class NotFoundError(Exception):
     def __init__(self, message):
         super().__init__(message)
         self.message = message
+        self.code = 404
 
     def __str__(self):
         return f"{self.message}"
